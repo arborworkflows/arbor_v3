77209cf2a41be8c49f21fe5bdf0c613aa9272c52
Jeff Baumes <jeff.baumes@kitware.com>
2020-12-03 16:56:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Terra Analytics
+
+## Docker
+
+Build container
+```
+docker build . -t terra-analytics
+```
+
+Run
+```
+docker run -p 9000:80 terra-analytics
+```
+
+Visit [localhost:9000](http://localhost:9000)
+
+## Development
+
+Install dependencies
+```
+pip install -r requirements.txt
+```
+
+Run
+```
+uvicorn main:app
+```
+
+Visit [localhost:8000](http://localhost:8000)
---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Response
+from fastapi.staticfiles import StaticFiles
+
+# import your apps
+import trait_explorer
+
+app = FastAPI()
+
+# initialize your apps
+trait_explorer.init(app)
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+@app.get('/')
+def index():
+    with open('index.html') as indexFile:
+        indexContent = indexFile.read()
+    
+    return Response(content=indexContent, media_type='text/html')
+
+
