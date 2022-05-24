3574c28d846749609bcdbbb0a505f31514ac538b
Curt Lisle <clisle@knowledgevis.com>
2022-05-24 14:02:26 -0400

    add test of R; experiment with how to use a methods.py file

--- fit_continuous/__init__.py
+++ fit_continuous/__init__.py
@@ -1,6 +1,16 @@
 from fastapi import Response
 import pandas as pd
 from tempfile import NamedTemporaryFile
+import rpy2
+from rpy2 import robjects
+
+#from methods import testRInterface
+
+def testRInterface():
+    r = robjects.r
+    r('''
+    print('howdy from R')
+    ''')
 
 
 def init(app):
@@ -16,9 +26,18 @@ def init(app):
     #     return Response(content=returnvalue, media_type='text/html')
 
 
+    # we still need:
+    # 1. read the tree and store in backend file system (/tmp)
+    # 2. read the table and (ditto)
+    # 3. Attach a method to the Go button  that calls R on the tree and table
+    # 3.5 (Kristen) build a 
+    # 4. reformat output to make a pretty picture (probaly return JSON )
+    # 5. in Javascript, make a Vega chart(s) 
+
     @app.get('/fit_continuous')
     def index():
         with open('fit_continuous/index.html') as indexFile:
             indexContent = indexFile.read()
         
+        testRInterface()
         return Response(content=indexContent, media_type='text/html')
