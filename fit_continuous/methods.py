f0abaa659d597973d9cdb6cc72af8efc1f76e9fd
Curt Lisle <clisle@knowledgevis.com>
2022-07-12 16:19:31 -0400

    fixed ability to return content files from R and read from python

--- fit_continuous/methods.py
+++ fit_continuous/methods.py
@@ -1,5 +1,10 @@
 import rpy2
+import pandas as pd
+import numpy as np
 from rpy2 import robjects
+import rpy2.robjects.numpy2ri as rpyn
+import os
+import json
 
 def testRInterface():
     r = robjects.r
@@ -73,5 +78,22 @@ dev.off()
 
     ''')
     print('** need to collect result from R here')
-    # Does not recognize the modelfit_summary_file variable
-    return modelfit_summary_file, plot_file
+    #print(env['result'])
+    #print(type(env['result']))
+ 
+    #valuesOnlyButNoKeys = np.asarray(env['result'])
+    result_df = pd.read_csv('/tmp/modelfile.csv')
+    print(result_df)
+
+    # repack from pandas dataframe into a dictionary
+    result = {}
+    result['nodes'] = ['howdy']
+    result['edges'] = ['doody']
+
+    # return the data arrays here as a JSON blob to javascript
+    # for javascript to render in vegalite 
+    returnString = json.dumps(result)
+    return returnString
+
+    # clean up the temporary files
+    # os.deletefile()
