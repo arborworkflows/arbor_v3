c57663c9372a46fb8201b8b6e54e5e0b45091918
kmartinet <33140392+kmartinet@users.noreply.github.com>
2022-12-15 23:30:56 -0800

    FitDiscrete returns data needed to plot the graph!

--- fit_discrete/methods.py
+++ fit_discrete/methods.py
@@ -34,6 +34,7 @@ def run_method(params):
     env['transform'] = transform
     env['modelfit_summary_file'] = '/tmp/modelfile.csv'
     env['plot_file'] = '/tmp/plotfile.png'
+    env['graph_file'] = '/tmp/graphfile.csv'
     r('''
 require(ape)
 require(geiger)
@@ -51,11 +52,83 @@ print(column)
 
 result <- fitDiscrete(phy, dat, model, transform)
 
-result <- t(as.data.frame(unlist(result$opt)))
-rownames(result) <- "Primary results"
-print(result)
+# Save transposed results as result CSV for user
+res <- t(as.data.frame(unlist(result$opt)))
+rownames(res) <- "Primary results"
+print(res)
 
-write.csv(result, modelfit_summary_file)
+write.csv(res, modelfit_summary_file)
+
+# Get coordinates and other information needed to plot in VEGA
+# First, find the names of the discrete characters
+names <- unique(dat[,column])
+
+# Find the number of discrete states
+len <- length(names)
+
+# The total number of transitions is states * (states-1)
+tot <- len * (len - 1)
+
+# Get the transition rates and AIC info
+transitions <- as.data.frame(unlist(result$opt))
+
+# The row names tell us the transition direction
+direction <- row.names(transitions)
+# We only need the first tot though
+direction <- direction[1:tot]
+
+# Split direction into 3 columns
+test <- strsplit(direction, "")
+direction_mat <- matrix(unlist(test), ncol = 3, byrow = T)
+direction_df <- as.data.frame(direction_mat)
+# Remove first column
+direction_df <- direction_df[,-1]
+
+# Grab all of the transition rates
+transitions <- transitions[1:tot,]
+# Add them to direction_df
+direction_df <- cbind(direction_df, transitions)
+# Change colnames to be more descriptive
+colnames(direction_df) <- c("From", "To", "TransRate")
+
+# Discrete categories are numbered in alpha order
+alpha_names <- sort(names)
+
+# In order to plot letters as points in VEGA, we need a key I think?
+# Thanks to https://stackoverflow.com/questions/40279052/coordinates-of-equally-distanced-n-points-on-a-circle-in-r
+#   for circle information
+points = exp(2i * pi * (1:len)/len)
+# Translate from complex to Cartesian plane
+cart_points = data.frame(x=Re(points), y = Im(points))
+# Create key dataframe
+key <- cbind(cart_points, alpha_names)
+
+# Create a VEGA-useable dataframe with points and info
+x1 <- 1:tot
+y1 <- 1:tot
+x2 <- 1:tot
+y2 <- 1:tot
+name1 <- 1:tot
+name2 <- 1:tot
+transrate <- direction_df$TransRate
+
+for(i in 1:tot) {
+  from <- direction_df[i,1] # ith row, 1st column
+  to <- direction_df[i,2] #ith row, 2nd column
+
+  x1[i] <- key[from,1] #from-th row, 1st column (x)
+  y1[i] <- key[from,2] #from-th row, 2nd column (y)
+  name1[i] <- key[from,3] #from-th row, 3rd column (alpha_name)
+
+  x2[i] <- key[to,1]
+  y2[i] <- key[to,2]
+  name2[i] <- key[to,3]
+}
+
+# Create final combined dataframe
+combo_df <- cbind(x1,y1,name1,x2,y2,name2,transrate)
+write.csv(combo_df, graph_file, row.names = FALSE)
+
     ''')
     print('** need to collect result from R here')
     #print(env['result'])
@@ -70,23 +143,20 @@ write.csv(result, modelfit_summary_file)
     result_as_dict = result_df.to_dict('records')
     print('result as dict:',result_as_dict)
 
-    # retreive the phenogram information from the R result
-    #connections_df = pd.read_csv('/tmp/pheno_connections.csv')
-    #points_df = pd.read_csv('/tmp/pheno_points.csv')
-    #connections_json = connections_df.to_dict('records')
-    #points_json = points_df.to_dict('records')
-    #print(points_json)
+    # retreive the graph information from the R result
+    graph_df = pd.read_csv('/tmp/graphfile.csv')
+    graph_json = graph_df.to_dict('records')
+    print(graph_json)
 
     # [{},{}]
     # json:  {'data': []}
 
     # repack from pandas dataframe into a dictionary.
     # values are returned as a list of one dictionary, so pick the first list entry
-    #result = {}
-    #result['points'] = points_json
-    #result['connections'] = connections_json
-    #for key in result_as_dict[0].keys():
-    #    result[key] = result_as_dict[0][key]
+    result = {}
+    result['graph'] = graph_json
+    for key in result_as_dict[0].keys():
+        result[key] = result_as_dict[0][key]
 
     # return the data arrays here as a JSON blob to javascript
     # for javascript to render in vegalite
